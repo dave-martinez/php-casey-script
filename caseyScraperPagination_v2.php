923d9910d27f5b13ec5d938e0ec7f3df8918de3d
Michael Dave Martinez <dave.martinez25@gmail.com>
2021-03-15 17:14:28 +0800

    Add upload to S3 functionality

--- caseyScraperPagination_v2.php
+++ caseyScraperPagination_v2.php
@@ -8,12 +8,44 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 
+function uploadFileToS3() {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
-// Load Environment Variables
-// Access via $_ENV['<variable>']
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+    if(
+        !(isset($_ENV['S3_REGION']) &&
+        isset($_ENV['S3_BUCKET']) &&
+        isset($_ENV['S3_SECRET_KEY']) &&
+        isset($_ENV['S3_ACCESS_KEY']))
+    ) {
+        throw new \Exception('Incomplete S3 credentials. Please check .env file.');
+    }
+ 
+    echo 'Uploading file to S3...' . PHP_EOL;
 
+    $s3 = new Aws\S3\S3Client([
+        'region'  => $_ENV['S3_REGION'],
+        'version' => 'latest',
+        'credentials' => [
+            'key'    => $_ENV['S3_ACCESS_KEY'],
+            'secret' => $_ENV['S3_SECRET_KEY'],
+        ]
+    ]);
+
+    $date = date('c');
+    $key = 'results_' . $date . '.json';
+
+    $result = $s3->putObject([
+        'Bucket' => $_ENV['S3_BUCKET'],
+        'Key'    => $key,
+        'SourceFile' => 'results.json',
+        'StorageClass' => 'REDUCED_REDUNDANCY',
+        'ACL'    => 'public-read',
+    ]);
+
+    echo 'Results JSON file link: ' . PHP_EOL . PHP_EOL . $result['ObjectURL'] . PHP_EOL . PHP_EOL;
+    echo 'If command + click on link gives you access denied, try copying and pasting the link to your browser.';
+}
 
 
 function getTableFromHTML($rawHTML) {
@@ -37,7 +69,7 @@ function getTableFromHTML($rawHTML) {
 }
 
 function serializeToJSON($obj) {
-    return json_encode($obj, JSON_UNESCAPED_SLASHES);
+    return json_encode($obj, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
 
 function getApplicationLink($applicationId) {
@@ -46,7 +78,7 @@ function getApplicationLink($applicationId) {
 
 
 
-function runScript($seleniumServer, $websiteUrl, $outputName) {
+function runScript($seleniumServer, $websiteUrl, $outputName, $uploadToS3) {
     $options = new ChromeOptions();
     $options->addArguments(['---headless']);
     $capabilitites = DesiredCapabilities::chrome();
@@ -111,22 +143,30 @@ function runScript($seleniumServer, $websiteUrl, $outputName) {
     fwrite($fp, $serializedJSON);
     fclose($fp);
     echo 'Results results successfully saved' . PHP_EOL;
-    echo 'Open file here: ' . realpath('./results.json') . PHP_EOL;
+    echo 'Open file here: ' . realpath('./results.json') . PHP_EOL . PHP_EOL;
+
+    if($uploadToS3) {
+        uploadFileToS3();
+    }
 }
 
 // Constants 
 $OUTPUT_FILENAME = 'results.json';
 $SELENIUM_SERVER = 'http://localhost:4444/wd/hub';
-$URL = 'https://eproperty.casey.vic.gov.au/T1PRProd/WebApps/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L14&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL14';
+$URL = 'https://eproperty.casey.vic.gov.au/T1PRProd/WebApps/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L7&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL7';
 
 try {
-    runScript($SELENIUM_SERVER, $URL, $OUTPUT_FILENAME);
+    $uploadToS3 = false;
+    runScript($SELENIUM_SERVER, $URL, $OUTPUT_FILENAME, $uploadToS3);
 } catch (Throwable $e) {
     $data = (object) ["code" => $e->getCode(), "message" => $e->getMessage(), "stackTrace" => $e->getTrace()];
     $serializedJSON =  serializeToJSON($data);
     $fp = fopen($OUTPUT_FILENAME, 'w');
     fwrite($fp, $serializedJSON);
     fclose($fp);
+
+    echo 'An error has occured: ' . PHP_EOL;
+    echo $e->getMessage();
 }
 
 
